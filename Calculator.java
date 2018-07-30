e621e138220dfede7d655417bcb6049404daf921
Vijay <vijaykumar.b15@iiits.in>
2018-07-30 10:51:23 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,53 @@
+/*In this project, you will use classes, methods, and objects to create a simple arithmetic calculator. The calculator will be able to:
+
+Add two integers
+Subtract two integers
+Multiply two integers
+Divide two integers
+Apply the modulo operator on two integers
+
+@BodaVijay
+*/
+
+public class Calculator{
+  
+ public Calculator(){
+   
+ }
+  public int add(int a,int b){
+    return a+b;
+  }
+  public int subtract(int a,int b){
+    return a-b;
+  }
+  public int multiply(int a,int b){
+    return a*b;
+  }
+  public int divide(int a,int b){
+    if (b==0){
+      System.out.println("Error! Dividing by zero is not allowed.");
+      return 0;
+    }
+    else{
+      return a/b;
+    }
+  }
+  public int modulo(int a,int b){
+      if (b==0){
+      System.out.println("Error! Dividing by zero is not allowed.");
+        return 0;
+    }
+    else{
+      return a%b;
+    }
+  }
+  public static void main(String[] args){
+    Calculator myCalculator=new Calculator();
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+    System.out.println(myCalculator.multiply(5,7));
+    System.out.println(myCalculator.divide(5,7));
+    System.out.println(myCalculator.modulo(5,7));
+    
+  }
+}
